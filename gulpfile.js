c15975735d2df2efddb8f75dacf4c2b5fb550dfa
vzamanillo <bellzebu@hotmail.com>
2016-06-20 19:03:15 +0200

    New gulpfile using nwjs-builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,48 @@
-var gulp = require('gulp');
-var NwBuilder = require('nw-builder');
-var os = require('os');
-var argv = require('yargs')
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  os = require('os'),
+  nwb = require('nwjs-builder'),
+  argv = require('yargs')
     .alias('p', 'platforms')
-    .argv;
-var del = require('del');
-var detectCurrentPlatform = require('nw-builder/lib/detectCurrentPlatform.js');
-var nw = new NwBuilder({
-    files: ['./src/**', './node_modules/**', './package.json'],
-    version: '0.15.3',
-    platforms: argv.p ? argv.p.split(',') : [detectCurrentPlatform()]
-}).on('log', console.log);
+    .argv,
+  del = require('del'),
+
+  detectCurrentPlatform = function(){
+    switch (process.platform) {
+      case 'darwin':
+          return process.arch === 'x64' ? 'osx64' : 'osx32';
+      case 'win32':
+          return (process.arch === 'x64' || process.env.hasOwnProerty('PROCESSOR_ARCHITEW6432')) ? 'win64' : 'win32';
+      case 'linux':
+          return process.arch === 'x64' ? 'linux64' : 'linux32';
+    }
+  };
 
 gulp.task('run', function() {
-    nw.options.files = './**';
-    return nw.run().catch(function(error) {
-        console.error(error);
+    nwb.commands.nwbuild([], {
+        run: true,
+        version: '0.15.3-sdk',
+        withFFmpeg: true
+    }, function (err, code) {
+        if(err) return gutil.log(gutil.colors.red.bold('[ERROR:]'),gutil.colors.bgRed(err));
+        else if(code === 0) gutil.log(gutil.colors.red.green('[OK:]'), 'Done');
     });
 });
 
 gulp.task('build', ['clean'], function() {
-    return nw.build().catch(function(error) {
-        console.error(error);
-    });
+  nwb.commands.nwbuild(['./'], {
+        version: '0.15.3',
+        platforms: argv.p ? argv.p.split(',') : detectCurrentPlatform(),
+        outputDir: './build',
+        withFFmpeg: true,
+        sideBySide: true,
+        production: true,
+        macIcns: './src/app/images/icons/popcorntime.icns',
+        winIco: './src/app/images/icons/popcorntime.ico'
+    }, function(err) {
+        if(err) return gutil.log(gutil.colors.red.bold('[ERROR:]'),gutil.colors.bgRed(err));
+        return gutil.log(gutil.colors.red.green('[OK:]'), 'Done');
+  });
 });
 
 gulp.task('clean', function() {
