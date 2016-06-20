0e832708c5c85d4cfdf6d11eb0896d4cd498d7c9
vzamanillo <bellzebu@hotmail.com>
2016-06-21 01:15:11 +0200

    gulpfile fixes, remove nw dependency in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,57 @@
 var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  os = require('os'),
-  nwb = require('nwjs-builder'),
-  argv = require('yargs')
-    .alias('p', 'platforms')
-    .argv,
-  del = require('del'),
-
-  detectCurrentPlatform = function(){
-    switch (process.platform) {
-      case 'darwin':
-          return process.arch === 'x64' ? 'osx64' : 'osx32';
-      case 'win32':
-          return (process.arch === 'x64' || process.env.hasOwnProerty('PROCESSOR_ARCHITEW6432')) ? 'win64' : 'win32';
-      case 'linux':
-          return process.arch === 'x64' ? 'linux64' : 'linux32';
-    }
-  };
+    gutil = require('gulp-util'),
+    os = require('os'),
+    del = require('del'),
+    nwb = require('nwjs-builder'),
+    argv = require('yargs')
+      .alias('p', 'platforms').argv,
+      detectCurrentPlatform = function(){
+        switch (process.platform) {
+          case 'darwin':
+              return process.arch === 'x64' ? 'osx64' : 'osx32';
+          case 'win32':
+              return (process.arch === 'x64' || process.env.hasOwnProerty('PROCESSOR_ARCHITEW6432')) ? 'win64' : 'win32';
+          case 'linux':
+              return process.arch === 'x64' ? 'linux64' : 'linux32';
+        }
+      },
+      logError = function(error, code){
+        return gutil.log(gutil.colors.red.bold('[ERROR:]'),gutil.colors.bgRed(err));
+      },
+      logSuccess = function(message){
+        return gutil.log(gutil.colors.red.green('[OK:]'), message);
+      };
 
 gulp.task('run', function() {
-    nwb.commands.nwbuild([], {
+    nwb.commands.nwbuild(['./'], {
         run: true,
         version: '0.15.3-sdk',
         withFFmpeg: true
     }, function (err, code) {
-        if(err) return gutil.log(gutil.colors.red.bold('[ERROR:]'),gutil.colors.bgRed(err));
-        else if(code === 0) gutil.log(gutil.colors.red.green('[OK:]'), 'Done');
+        if(err) return handleError(err);
+        else if(code === 0) return logSuccess('Done');
+        else return handleError('Unexpected error', code);
     });
 });
 
 gulp.task('build', ['clean'], function() {
-  nwb.commands.nwbuild(['./'], {
+  nwb.commands.nwbuild('./', {
         version: '0.15.3',
-        platforms: argv.p ? argv.p.split(',') : detectCurrentPlatform(),
-        outputDir: './build',
+        platforms: argv.p ? argv.p : detectCurrentPlatform(),
         withFFmpeg: true,
-        production: true,
+        production: false,
         macIcns: './src/app/images/icons/popcorntime.icns',
-        winIco: './src/app/images/icons/popcorntime.ico'
+        winIco: './src/app/images/icons/popcorntime.ico',
+        sideBySide: false,
+        includes: [
+            ['./', 'package.json', './'],
+            ['./', 'LICENSE.txt', './'],
+            // cp -r ./src/ ${DIR_BUILD}/
+            ['./', 'src/**/**', './']
+        ],
     }, function(err) {
-        if(err) return gutil.log(gutil.colors.red.bold('[ERROR:]'),gutil.colors.bgRed(err));
-        return gutil.log(gutil.colors.red.green('[OK:]'), 'Done');
+        if(err) return handleError(err);
+        return logSuccess('Done');
   });
 });
 
