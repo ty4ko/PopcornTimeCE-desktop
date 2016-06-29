a1019261ec67ef086b9ae47ef9d64977d782bb12
vzamanillo <bellzebu@hotmail.com>
2016-06-29 22:46:48 +0200

    Added missing js map files, removed get in subs model, fix for vdata
    Removed get in subs model to avoid 'Not implemented in parent model' backbone error.
    The vdata error was not fixed in main dev video.js

--- src/vendor/video.js/dist/video-js/video.dev.js
+++ src/vendor/video.js/dist/video-js/video.dev.js
@@ -6601,6 +6601,9 @@ vjs.MediaTechController.prototype.dispose = function() {
 
   if (this.manualTimeUpdates) { this.manualTimeUpdatesOff(); }
 
+  // Dispose needs to clear currentTimeInterval to avoid vdata error (https://github.com/videojs/video.js/issues/1484#issuecomment-55245716)
+  clearInterval(this.currentTimeInterval);
+
   vjs.Component.prototype.dispose.call(this);
 };
 
